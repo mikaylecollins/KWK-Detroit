c1f926b06b44b11cac93d1867863b7bb5f7fadb7
JohannaJachna <jgjachna@gmail.com>
2019-08-06 13:27:49 -0400

    update

--- Collections.playground/Contents.swift
+++ Collections.playground/Contents.swift
@@ -1,3 +1,55 @@
-import UIKit
+////Arrays
+//var FriendsOfKarlie = ["sumaia", "t swift", "maddie", "Selena"]
+//
+//print(FriendsOfKarlie[2])
+//
+//(FriendsOfKarlie[2]) = "Elmo"
+//print(FriendsOfKarlie)
+//
+//FriendsOfKarlie.remove(at: 1)
+//print(FriendsOfKarlie)
+//
+//var FriendsOfKayle = ["Mom","Lexi","Jina"]
+//
+//print(FriendsOfKayle[2])
+//
+//FriendsOfKayle.append("Robin")
+//
+//(FriendsOfKayle[0]) = "Corinee"
+//
+//FriendsOfKayle.remove(at: 0)
+//print(FriendsOfKayle)
+//
+//for friend in FriendsOfKayle {
+//    print("Hello \(friend)")
+//}
 
-var str = "Hello, playground"
+                    //Dictionaries
+
+//var perfectTen: [String : String] =
+//[:]
+//print(perfectTen)
+
+//var perfectEleven = ["almond flour" : "3 and 1/2 cups", "gluten-free oats" : "2 cups", "dark chocolate chips" : "1 cup"]
+//
+//// almond flour, oats, and choc.chips are all our keys
+//print(perfectEleven)
+//
+//perfectEleven["dark chocolate chips"] = "2 cups"
+//print(perfectEleven)
+//
+//print(perfectEleven.keys)
+//print(perfectEleven.values)
+//
+//print(perfectEleven["gluten-free oats"]!)
+//
+//perfectEleven["vanilla"] = "1 tsp"
+//print(perfectEleven)
+//
+//perfectEleven["gluten-free oats"] = nil
+//print(perfectEleven)
+
+var familytree = ["Mother" : "Sherri", "Father" : "Billy", "Sister" : "Madison", "Pet1" : "Jack", "Pet2" : "Bea"]
+print(familytree)
+
+
