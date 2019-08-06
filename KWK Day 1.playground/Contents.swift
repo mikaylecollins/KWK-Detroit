30d4e093b2aae0e4a8b0c97ed1aaafd6f881f13d
JohannaJachna <jgjachna@gmail.com>
2019-08-06 10:02:20 -0400

    initial commit

---
+++ KWK Day 1.playground/Contents.swift
@@ -0,0 +1,156 @@
+//import UIKit
+//
+//let greetingSayHello = "Hi!"
+////let is a constant (always stays the same)
+//
+//var namefirst = "kayle"
+////string interpolation
+//print("\(greetingSayHello) \(namefirst)!")
+//////greetingSayHello = "hola"
+//
+//var first = "Karlie"
+//var last = "Kloss"
+//var greeting = "I Love"
+//print("\(first)\(last)")
+//
+//print("\(last)\(first)")
+//
+////print("\(greeting) \(first)")
+//
+//var integer = 4.0
+//var double = 5.0
+//
+//print(integer * double)
+//
+//var a = 12
+//var b = 65
+//var c = 31
+//var d = 98
+//
+//
+//print(Double((a + b + c + d)) / 4)
+//
+//var a = 10.25
+//var b = 20.0
+//
+//print(a * b)
+//
+//var name = "kayle"
+//var birthday = "Happy birthday to"
+//
+//print("\(birthday) \(name)")
+
+//CONDITIONALS
+//comparison operators
+//5 < 3
+//
+//12 > 7
+//
+//
+//6 != 8
+//7 == "7"
+//
+//7 == 7
+//
+//var luckyNum = 7
+//
+//luckyNum < 10
+
+//if, else if statements
+//
+//var dogAge = 10
+//
+//if dogAge < 2 {
+//    print("you are a puppy")
+//}
+//    else if dogAge > 12 {
+//        print("you are elderly")
+//}
+//else {
+//    print("that's ruff")
+//}
+
+//var favfood = "culvers"
+//
+//if favfood == "chipotle" {
+//    print("no gross")
+//}
+//else if favfood == "starbucks" {
+//    print("its good")
+//}
+//
+//else if favfood == "red robin" {
+//    print("Eat it!")
+//}
+//
+////else {
+//    print("try it")
+//}
+//var hasAccount = true
+//
+//if hasAccount == false {
+//    print("let's get some info to create an account")
+//}
+//else if hasAccount == true {
+//    print("log in!")
+//}
+
+//var grade = 4
+//
+//if grade < 5 {
+//    print("elementary school")
+//}
+//else if grade > 9 {
+//    print("high school")
+//}
+//else {
+//  print("middle school")
+//}
+//func is a keyword that represents the start of a func
+//func walkDog () {
+//    print("get leash and bag for dog")
+//        print("attach leash to dog")
+//            print("go outside to walk dog")
+//}
+//
+//walkDog()      //called func named walkDog()
+//
+//func cereal () {
+//    print("retrive bowl from cabinet")
+//        print("pour milk in bowl")
+//            print("pour cereal")
+//                print("use spoon and eat")
+//
+//}
+//
+//func hello(name : String) {
+//    print("Hello \(name)")
+//}
+//    hello(name : "trinity")
+//
+//func walkDog(numberOfDogs : Int) -> Int {
+//    var lengthOfWalk = numberOfDogs * 15
+//    return lengthOfWalk
+//}
+//walkDog(numberOfDogs : 4)
+
+//func robot(numberOfBreadSlices: Int)  {
+//    print("Get \(numberOfBreadSlices) pieces of bread")
+//    print("Get out penut butter and jelly")
+//    print("spread the penut butter and jelly on bread")
+//    print("put together bread and enjoy!")
+//}
+//robot(numberOfBreadSlices : 4)
+
+//func pizza(toppings : Int ) {
+//    print("I want a cheese pizza with \(toppings) pieces of peperoni on it")
+//}
+//pizza(toppings : 6)
+//
+//func people(numberOfFriends : Int) -> Int {
+//    var SlicesOfPizza = numberOfFriends * 2
+//    return SlicesOfPizza
+//   // print(SlicesOfPizza)
+//}
+//let peopleAtParty = people(numberOfFriends : 4)
+//print("I need \(peopleAtParty) slices of pizza for my 4 friends")
